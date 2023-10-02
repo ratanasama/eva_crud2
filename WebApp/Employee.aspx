60c8bf1799f4220ce926177d4c31a20ccf8f66cf
ratanasama <krispradanaratana@gmail.com>
2023-10-02 15:33:12 +0700

    first commit

---
+++ WebApp/Employee.aspx
@@ -0,0 +1,143 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employee.aspx.cs" Inherits="WebApp.Employee" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .newStyle1 {
+            font-family: "Poppins Medium";
+        }
+        .newStyle2 {
+            font-family: "Poppins Medium";
+        }
+        .auto-style1 {
+            height: 32px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2 style="color: #0000FF">Employee Form</h2>
+    <div>
+        <table>
+            <tr>
+                <td>
+                    <asp:Label ID="Label1" runat="server" Text="Nama" CssClass="newStyle1"></asp:Label>
+                </td>
+                <td>
+                     <asp:TextBox ID="txtNama" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Label2" runat="server" Text="NIK / ID" CssClass="newStyle1"></asp:Label>
+                </td>
+                <td>
+                     <asp:TextBox ID="txtID" runat="server" onkeypress="if(event.keyCode<48 || event.keyCode>57)event.returnValue=false;"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">
+                    <asp:Label ID="Label3" runat="server" Text="Usia" CssClass="newStyle1"></asp:Label>
+                </td>
+                <td class="auto-style1">
+                     <asp:TextBox ID="txtUsia" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                
+                <td>
+                    <asp:Label ID="Label5" runat="server" Text="Tanggal Masuk Kerja" CssClass="newStyle1"></asp:Label>
+                </td>
+                <td>    
+                    <asp:TextBox ID="txtDateTime" runat="server" ReadOnly="false"></asp:TextBox>
+                    <asp:Calendar ID="calDateTime" runat="server" OnSelectionChanged="calDateTime_SelectionChanged"></asp:Calendar>
+
+                    <asp:DropDownList ID="ddlDay" runat="server" AutoPostBack="True"></asp:DropDownList>
+                    <asp:DropDownList ID="ddlMonth" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlMonth_SelectedIndexChanged"></asp:DropDownList>
+                    <asp:DropDownList ID="ddlYear" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlYear_SelectedIndexChanged" ViewStateMode="Enabled"></asp:DropDownList>
+                   
+                </td>
+            </tr>
+                        
+            <tr>
+                <td>
+                    <asp:Button ID="btnSave" runat="server" OnClick="btnSave_Click" Text="Save" CssClass="newStyle2" style="height: 45px"/>
+                </td>
+                <td>
+                    <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" CssClass="newStyle2"/>
+                </td>
+                <td>
+                    <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>     
+                                            
+                </td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <asp:Label ID="lblUpdate" runat="server" Text="" Visible="false"></asp:Label>  
+        <asp:Label ID="lblID" runat="server" Text="" Visible="false"></asp:Label> 
+        <asp:GridView ID="grid1" runat="server" AutoGenerateColumns="False" CellPadding="4" ForeColor="#333333" GridLines="None" OnSelectedIndexChanged="grid1_SelectedIndexChanged" Width="833px" >  
+     <AlternatingRowStyle BackColor="White" />  
+     <columns>  
+         <asp:TemplateField HeaderText="ID" Visible="false">  
+             <ItemTemplate>  
+                 <asp:Label ID="lblId" runat="server" Text='<%#Bind("IDKaryawan") %>'></asp:Label>  
+             </ItemTemplate>  
+         </asp:TemplateField>  
+         <asp:TemplateField HeaderText="NIK">
+    <ItemTemplate>  
+        <asp:Label ID="lblNIK" runat="server" Text='<%#Bind("IDKaryawan") %>'></asp:Label>  
+    </ItemTemplate>  
+</asp:TemplateField>    
+         <asp:TemplateField HeaderText="Nama">  
+             <ItemTemplate>  
+                 <asp:Label ID="lblNama" runat="server" Text='<%#Bind("NmKaryawan") %>'></asp:Label>  
+             </ItemTemplate>  
+         </asp:TemplateField>           
+         <asp:TemplateField HeaderText="Tanggal Masuk Kerja">  
+             <ItemTemplate>  
+                 <asp:Label ID="lblTglMasukKerja" runat="server" Text='<%#Bind("TglMasukKerja") %>'></asp:Label>  
+             </ItemTemplate>  
+         </asp:TemplateField>
+         <asp:TemplateField HeaderText="Usia">  
+             <ItemTemplate>  
+                 <asp:Label ID="lblUsia" runat="server" Text='<%#Bind("Usia") %>'></asp:Label>  
+             </ItemTemplate>  
+         </asp:TemplateField>  
+         
+         <asp:TemplateField HeaderText="Edit">  
+             <ItemTemplate>  
+                 <asp:LinkButton ID="btnEdit" runat="server" CommandArgument='<%#Eval("IDKaryawan")%>' OnCommand="btnEdit_Command" Text="Edit">
+                      </asp:LinkButton>
+             </ItemTemplate>  
+         </asp:TemplateField>  
+         <asp:TemplateField HeaderText="Hapus">  
+             <ItemTemplate>  
+                 <asp:LinkButton ID="btnDelete" runat="server" CommandArgument='<%#Eval("IDKaryawan")%>' OnCommand="btnDelete_Command" Text="Delete">
+                      </asp:LinkButton>
+             </ItemTemplate>  
+         </asp:TemplateField>  
+     </columns>  
+     <EditRowStyle BackColor="#2461BF" />  
+     <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />  
+     <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />  
+     <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />  
+     <RowStyle BackColor="#EFF3FB" />  
+     <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />  
+     <SortedAscendingCellStyle BackColor="#F5F7FB" />  
+     <SortedAscendingHeaderStyle BackColor="#6D95E1" />  
+     <SortedDescendingCellStyle BackColor="#E9EBEF" />  
+     <SortedDescendingHeaderStyle BackColor="#4870BE" />  
+ </asp:GridView> 
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApp/Employee.aspx.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApp
+{
+    public partial class Employee : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        public int Id;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //string connection = @"Data Source=DESKTOP-0TD75N5\SQLEXPRESS;Initial Catalog=TestDB;User=sa;password=sa12345";
+            string connection = @"Data Source=DESKTOP-FU2SNPG;Initial Catalog=db_hris;Integrated Security=True";
+            conn = new SqlConnection(connection);
+            p_initDdl();
+            DisplayRecord();
+        }
+        private void p_initDdl()
+        {
+            ddlMonth.Items.Clear();
+            ddlDay.Items.Clear();
+            ddlYear.Items.Clear();
+
+            for (int i = 0; i < 31; i++)
+            {
+                ddlDay.Items.Add((i + 1).ToString());
+            }
+
+            ddlMonth.Items.Add("January");
+            ddlMonth.Items.Add("February");
+            ddlMonth.Items.Add("March");
+            ddlMonth.Items.Add("April");
+            ddlMonth.Items.Add("May");
+            ddlMonth.Items.Add("June");
+            ddlMonth.Items.Add("July");
+            ddlMonth.Items.Add("Augusts");
+            ddlMonth.Items.Add("September");
+            ddlMonth.Items.Add("October");
+            ddlMonth.Items.Add("November");
+            ddlMonth.Items.Add("December");           
+
+            for (int i = 1944; i < 2099; i++)
+            {
+                ddlYear.Items.Add((i + 1).ToString());
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            p_SaveOrUpdate_BARU();
+            DisplayRecord();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            p_UpdateData();
+            DisplayRecord();
+
+            //string dateBirth = ddlYear.Text + "-" + (ddlMonth.SelectedIndex + 1).ToString() + "-" + ddlDay.Text;
+            //string dateBirth = ddlYear.Text.ToString();
+            //lblMessage.Text = dateBirth;
+
+        }
+        private void p_ClearData()
+        {
+            
+            txtNama.Text = string.Empty;
+            txtID.Text = string.Empty;
+            txtUsia.Text = string.Empty;
+            ddlDay.SelectedIndex = 0;
+            ddlMonth.SelectedIndex = 0;
+            ddlYear.SelectedIndex = 0;
+            //rdLaki.Checked = false;
+            //rdPerempuan.Checked = false;
+            lblUpdate.Text = string.Empty;
+        }
+        public DataTable DisplayRecord()
+        {
+            conn.Open();
+            //SqlDataAdapter Adp = new SqlDataAdapter("select * from TblEmployee", conn);
+            SqlDataAdapter Adp = new SqlDataAdapter("select * from Tbl_EMPLOYEE_EVA", conn);
+            DataTable Dt = new DataTable();
+            Adp.Fill(Dt);
+            grid1.DataSource = Dt;
+            grid1.DataBind();
+            conn.Close();
+            return Dt;
+            
+        }
+
+        private void p_UpdateData()
+        {
+            try
+            {              
+                if (txtID.Text == string.Empty ||
+                    txtUsia.Text == string.Empty ||
+                    txtNama.Text == string.Empty ||
+                    txtUsia.Text == string.Empty)
+                {
+                    lblMessage.Text = "harap isi semua Data";
+                    return;
+                }
+                //if (rdLaki.Checked == false && rdPerempuan.Checked == false)
+                //{
+                //    lblMessage.Text = "harap pilih Jenis Kelamin";
+                //    return;
+                //}
+
+                conn.Open();
+                //string dateBirth = ddlYear.Text + "-" + (ddlMonth.SelectedIndex + 1).ToString() + "-" + ddlDay.Text;
+                string dateBirth = txtDateTime.Text;
+                Id = int.Parse(lblID.Text);             
+
+                SqlCommand cmd = new SqlCommand(@"update Tbl_EMPLOYEE_EVA set NmKaryawan='" + txtNama.Text +  "', Usia='" +
+                       txtUsia.Text + "' where IDKaryawan ='" + Id+ "'", conn);
+
+                //SqlCommand cmd = new SqlCommand(@"update tb_employeeTaf set nama='" + txtNama.Text + "', nik ='" + txtNIK.Text + "', posisi='" +
+                //       txtPosisi.Text + "', divisi ='" + txtDivisi.Text + "', jenis_kelamin ='" + type + "'", conn);
+
+
+                cmd.ExecuteNonQuery();
+                lblMessage.Text = "Success edit Data";
+
+
+                conn.Close();
+                p_ClearData();
+            }
+            
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+        }
+        
+
+        private void p_SaveOrUpdate_BARU()
+        {
+            try
+            {
+                //int IDEmployee = 0;
+                //if (lblID.Text != string.Empty)
+                //{
+                //    IDEmployee = int.Parse(lblID.Text);
+                //}
+
+                if (txtID.Text == string.Empty ||                   
+                    txtNama.Text == string.Empty ||
+                    txtUsia.Text == string.Empty)
+                {
+                    lblMessage.Text = "harap isi semua Data";
+                    return;
+                }
+               
+
+                conn.Open();
+                //string dateBirth = ddlYear.Text + "-" + (ddlMonth.SelectedIndex + 1).ToString() + "-" + ddlDay.Text;
+                string dateBirth = txtDateTime.Text;
+                //string type;
+                
+                //SqlCommand cmd = new SqlCommand("CreateOrEditTblEmployee", conn);
+                SqlCommand cmd = new SqlCommand(@"insert into Tbl_EMPLOYEE_EVA (IDKaryawan, NmKaryawan, TglMasukKerja, Usia) 
+                values (@IDKaryawan, @NmKaryawan, @TglMasukKerja, @Usia)", conn);
+                //cmd.CommandType = CommandType.StoredProcedure;
+
+
+                cmd.Parameters.AddWithValue("@IDKaryawan", txtID.Text);
+                cmd.Parameters.AddWithValue("@NmKaryawan", txtNama.Text);
+                cmd.Parameters.AddWithValue("@TglMasukKerja", dateBirth);
+                cmd.Parameters.AddWithValue("@Usia", txtUsia.Text);
+               
+                cmd.ExecuteNonQuery();
+                lblMessage.Text = "Success save Data";
+
+
+                conn.Close();
+                p_ClearData();
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+
+        }
+
+        protected void btnDelete_Command(object sender, System.Web.UI.WebControls.CommandEventArgs e)
+        {
+            conn.Open();
+            int Id = Int32.Parse(e.CommandArgument.ToString());
+            string query = "delete from Tbl_EMPLOYEE_EVA where IDKaryawan='" + Id+"'";
+            SqlCommand command = new SqlCommand(query, conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+            lblMessage.Text = "Sukses Hapus Data";
+            DisplayRecord();
+        }
+
+        protected void btnEdit_Command(object sender, System.Web.UI.WebControls.CommandEventArgs e)
+        {
+            conn.Open();
+            Id = Int32.Parse(e.CommandArgument.ToString());
+            lblUpdate.Text = "Update";
+            lblID.Text = Id.ToString();
+            SqlDataAdapter Adp = new SqlDataAdapter("select * from Tbl_EMPLOYEE_EVA where IDKaryawan='" + Id +"'", conn);
+            DataTable dt = new DataTable();
+            Adp.Fill(dt);
+
+            //txtID.Text = dt.Rows[0]["NIK"].ToString();
+            //txtNama.Text = dt.Rows[0]["Nama"].ToString();
+            //DateTime dtm = DateTime.Parse(dt.Rows[0]["Tanggal Masuk Kerja"].ToString());
+            //txtUsia.Text = dt.Rows[0]["Usia"].ToString();
+            //txtDateTime.Text = dtm.ToString();
+
+            txtID.Text = dt.Rows[0][0].ToString();
+            txtNama.Text = dt.Rows[0][1].ToString();
+            DateTime dtm = DateTime.Parse(dt.Rows[0][2].ToString());
+            txtUsia.Text = dt.Rows[0][3].ToString();
+            txtDateTime.Text = dtm.ToString();
+
+            conn.Close();
+        }
+
+        protected void rdLaki_CheckedChanged(object sender, EventArgs e)
+        {
+            //rdPerempuan.Checked = false;
+        }
+
+        protected void rdPerempuan_CheckedChanged(object sender, EventArgs e)
+        {
+            //rdLaki.Checked = false;
+        }
+
+        protected void grid1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void calDateTime_SelectionChanged(object sender, EventArgs e)
+        {
+            //txtDateTime.Text = calDateTime.SelectedDate.ToString("yyyy-MM-dd HH:mm:ss");
+            txtDateTime.Text = calDateTime.SelectedDate.ToString("yyyy-MM-dd");
+            //txtDateTime.Text = calDateTime.SelectedDate.ToString("dd/MM/yyyy HH:mm:ss");
+            //txtDateTime.Text = calDateTime.SelectedDate.ToString("dd/MM/yyyy");
+        }
+
+        protected void ddlMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void ddlYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedText = ddlYear.Text;
+            txtDateTime.Text = selectedText;
+        }
+    }
+}
